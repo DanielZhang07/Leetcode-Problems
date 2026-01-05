cfb91d5b25b83a28ef93ed33a3919415e4ac50da
Daniel0717 <daniel.zhang0717@gmail.com>
2026-01-05 16:06:09 -0500

    commit 1 inital commit 2026-01-05

---
+++ problems/p1-twosum.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    print("hello")
+    def twoSum(self, nums, target) -> List[int]:
+        found: dict = {}
+        for (i, v) in enumerate(nums):
+            if (target - v) in found:
+                return [found[target - v], i]
+            else:
+                found[v] = i
+
+
+sol = Solution()
+answer = sol.twoSum([2, 7, 11, 15], 9)
+print(answer)
